eadb66b150bbc624fee88e951f1c86a278c80213
Martijn Vegter <martijn@martijnvegter.com>
2017-03-09 15:48:27 +0100

    Added proper travis support

---
+++ test/app.js
@@ -0,0 +1,18 @@
+const ZermeloSchedule = require('../lib/index.js'); // use require('zermelo') in production
+const Zermelo         = new ZermeloSchedule();
+
+
+Zermelo.logOn({
+  'school'    : process.env.school, 
+  'username'  : process.env.username,
+  'password'  : process.env.password
+});
+
+Zermelo.on('loggedOn', function() {
+  Zermelo.getSchedule('1489016804', '1489276004', function(err, res) {
+    if(err) {
+    	throw new Error(err);
+    }
+    console.log(res);
+  });
+});
