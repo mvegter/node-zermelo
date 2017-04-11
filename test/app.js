cbdbd34a610656dcae681c3e2e6898b83e8102d1
Martijn Vegter <mvegter.coding@gmail.com>
2017-04-11 19:24:30 +0200

    Change: Standard to SemiStandard

--- test/app.js
+++ test/app.js
@@ -1,89 +1,88 @@
 const ZermeloSchedule = require('../lib/index.js');
-const Zermelo1    = new ZermeloSchedule(process.env.school);
+const Zermelo1 = new ZermeloSchedule(process.env.school);
 
 Zermelo1.logOn({
-  'username'  : process.env.username,
-  'password'  : process.env.password
+  'username': process.env.username,
+  'password': process.env.password
 }, (err) => {
-  if(err) {
+  if (err) {
     console.error(err);
   }
   console.log('Logged in!');
 
-  var curr = new Date;
+  var curr = new Date();
   var monday = new Date(curr.setDate(curr.getDate() - curr.getDay()));
-  var friday = new Date(curr.setDate(curr.getDate() - curr.getDay()+6));
+  var friday = new Date(curr.setDate(curr.getDate() - curr.getDay() + 6));
 
   monday = Math.round(monday.getTime() / 1000);
   friday = Math.round(friday.getTime() / 1000);
 
   Zermelo1.getSchedule(monday, friday, (err, res) => {
-    if(err) {
+    if (err) {
       console.log(err);
     }
     console.log('Got schedule!');
   });
 
   Zermelo1.getSchedule('asdf', friday, (err, res) => {
-    if(err) {
+    if (err) {
       console.log(err);
     }
     console.log('Got schedule!');
   });
 
   Zermelo1.getSchedule(monday, 'asdf', (err, res) => {
-    if(err) {
+    if (err) {
       console.log(err);
     }
     console.log('Got schedule!');
-  });     
+  });
 });
 
-const Zermelo2    = new ZermeloSchedule(process.env.school, true);
-const Zermelo3    = new ZermeloSchedule(process.env.school, 'true');
-const Zermelo5    = new ZermeloSchedule(process.env.school, false);
-const Zermelo6    = new ZermeloSchedule(process.env.school, 'false');
+const Zermelo2 = new ZermeloSchedule(process.env.school, true);
+const Zermelo3 = new ZermeloSchedule(process.env.school, 'true');
+const Zermelo5 = new ZermeloSchedule(process.env.school, false);
+const Zermelo6 = new ZermeloSchedule(process.env.school, 'false');
 
-const Zermelo7    = new ZermeloSchedule({'school': process.env.school});
-const Zermelo8    = new ZermeloSchedule({'School': process.env.school});
+const Zermelo7 = new ZermeloSchedule({'school': process.env.school});
+const Zermelo8 = new ZermeloSchedule({'School': process.env.school});
 
-const Zermelo9    = new ZermeloSchedule({'school': process.env.school, 'secure': true});
-const Zermelo10    = new ZermeloSchedule({'school': process.env.school, 'secure': 'true'});
-const Zermelo11   = new ZermeloSchedule({'school': process.env.school, 'secure': false});
-const Zermelo12   = new ZermeloSchedule({'school': process.env.school, 'secure': 'false'});
+const Zermelo9 = new ZermeloSchedule({'school': process.env.school, 'secure': true});
+const Zermelo10 = new ZermeloSchedule({'school': process.env.school, 'secure': 'true'});
+const Zermelo11 = new ZermeloSchedule({'school': process.env.school, 'secure': false});
+const Zermelo12 = new ZermeloSchedule({'school': process.env.school, 'secure': 'false'});
 
-const Zermelo13   = new ZermeloSchedule({'school': process.env.school});
+const Zermelo13 = new ZermeloSchedule({'school': process.env.school});
 Zermelo13.logOn({
-  'password'  : process.env.password
+  'password': process.env.password
 }, (err) => {
-  if(err) {
+  if (err) {
     console.log(err);
   }
 });
 
 Zermelo13.logOn({
-  'username'  : process.env.username
+  'username': process.env.username
 }, (err) => {
-  if(err) {
+  if (err) {
     console.log(err);
   }
 });
 
 Zermelo13.logOn({
-  'username'  : process.env.username,
-  'password'  : process.env.password
+  'username': process.env.username,
+  'password': process.env.password
 }, (err) => {
-  if(err) {
+  if (err) {
     console.log(err);
   }
 
   Zermelo13.logOn({
-    'username'  : process.env.username,
-    'password'  : process.env.password
+    'username': process.env.username,
+    'password': process.env.password
   }, (err) => {
-    if(err) {
+    if (err) {
       console.log(err);
     }
   });
 });
-
