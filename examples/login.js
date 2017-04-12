e72c310807d7fa2c8b7f4bd7cb559c058b81ba32
Martijn Vegter <martijn@martijnvegter.com>
2017-04-12 15:07:44 +0200

    Fixed Examples

--- examples/login.js
+++ examples/login.js
@@ -1,7 +1,8 @@
-const Schedule = require('zermelo');
-const Zermelo = new Schedule('SCHOOL_NAME');
+const Schedule = require('../lib/index.js'); // use require('zermelo') in production
+const Zermelo = new Schedule();
 
 Zermelo.logOn({
+  'school': 'SCHOOL',
   'username': 'USERNAME',
   'password': 'PASSWORD'
 }, (err) => {
